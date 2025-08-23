2c0fd6d032f8c4003bc5dac874474b6d7f040b4e
Harshit Singh Parmar <hsp9696@gmail.com>
2025-08-23 23:04:04 +0530

    Adding a readme

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 process.on('uncaughtException', (err) => {
   console.log('UNHANDLED REJECTION Shutting down....');
   console.log(err.name, err.message);
-    process.exit(1);
+  process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
