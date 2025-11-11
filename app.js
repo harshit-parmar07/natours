a888ece2f6c1a629fe0047204b1a9589b704a695
Harshit Singh Parmar <hsp9696@gmail.com>
2025-11-11 20:56:49 +0530

    Refactor: app.js

--- app.js
+++ app.js
@@ -28,25 +28,16 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1)GLOBAL MIDDLEWARES
-// app.use(cors());
-
-app.use(
-  cors({
-    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
-    credentials: true
-  })
-);
-
+app.use(cors());
 app.options('*', cors());
 
-// // app.js
+// for development testing
 // app.use(
 //   cors({
 //     origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
 //     credentials: true
 //   })
 // );
-// app.options('*', cors());
 
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -78,23 +69,6 @@ app.use(
 
 //   res.setHeader('Content-Security-Policy', csp);
 //   next();
-
-//   // const csp = [
-//   //   "default-src 'self'",
-//   //   "script-src 'self' https://api.mapbox.com https://js.stripe.com https://m.stripe.network blob:",
-//   //   "style-src 'self' https://fonts.googleapis.com https://api.mapbox.com 'unsafe-inline'",
-//   //   "img-src 'self' data: blob: https://m.stripe.network https://api.mapbox.com",
-//   //   "font-src 'self' https://fonts.gstatic.com data:",
-//   //   "connect-src 'self' http://127.0.0.1:3000 http://localhost:3000 https://api.mapbox.com https://events.mapbox.com https://api.stripe.com https://m.stripe.network ws://127.0.0.1:* ws://localhost:*",
-//   //   'frame-src https://js.stripe.com',
-//   //   "worker-src 'self' blob:",
-//   //   "object-src 'none'",
-//   //   "base-uri 'self'",
-//   //   "frame-ancestors 'none'"
-//   // ].join('; ');
-//   // res.setHeader('Content-Security-Policy', csp);
-//   // next();
-// });
 
 // DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === 'development') {
@@ -146,20 +120,20 @@ app.use(compression());
 
 // 3) ROUTES
 
-app.use((req, res, next) => {
-  if (req.method === 'HEAD') {
-    return res.status(200).end();
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (req.method === 'HEAD') {
+//     return res.status(200).end();
+//   }
+//   next();
+// });
 
-app.get('/.well-known/appspecific/com.chrome.devtools.json', (req, res) => {
-  // Either no content:
-  // return res.status(204).end();
+// app.get('/.well-known/appspecific/com.chrome.devtools.json', (req, res) => {
+//   // Either no content:
+//   // return res.status(204).end();
 
-  // Or minimal JSON:
-  res.type('application/json').send('{}');
-});
+//   // Or minimal JSON:
+//   res.type('application/json').send('{}');
+// });
 
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
